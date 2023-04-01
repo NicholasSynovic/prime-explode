93afabec451d9e4d5a1d3c6d3afe87d02b82d825
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-04-01 11:46:04 -0500

    Format files

--- prime_explode/args.py
+++ prime_explode/args.py
@@ -1,15 +1,44 @@
-from argparse import Namespace, ArgumentParser, _SubParsersAction
+from argparse import ArgumentParser, Namespace, _SubParsersAction
 from importlib.metadata import version
 from pathlib import Path
 
-def getArgs()   ->  Namespace:
-    parser: ArgumentParser = ArgumentParser(prog="PRIME Explode", usage="prime-explode", description="To explode a code repository into a directories organized by commit", epilog="Authors: Nicholas M. Synovic")
 
-    subParsers: _SubParsersAction = parser.add_subparsers(title="VCS Options", help="Types of VCS that are supported by this tool")
-    parser.add_argument("-v", "--verson", action="version", version=f"PRIME Explode {version(distribution_name='prime-explode')}")
+def getArgs() -> Namespace:
+    parser: ArgumentParser = ArgumentParser(
+        prog="PRIME Explode",
+        usage="prime-explode",
+        description="To explode a code repository into a directories organized by commit",
+        epilog="Authors: Nicholas M. Synovic",
+    )
 
-    gitParser: ArgumentParser = subParsers.add_parser(name="git", help="Options for interfacing with Git repositories")
-    gitParser.add_argument("-s", "--src-directory", default=Path(".").resolve(), type=Path, required=False, help="Directory containing the repository to explode")
-    gitParser.add_argument("-d", "--dest-directory", default=Path(".").resolve(), type=Path, required=False, help="Directory to explode into")
+    subParsers: _SubParsersAction = parser.add_subparsers(
+        title="VCS Options", help="Types of VCS that are supported by this tool"
+    )
+    parser.add_argument(
+        "-v",
+        "--verson",
+        action="version",
+        version=f"PRIME Explode {version(distribution_name='prime-explode')}",
+    )
+
+    gitParser: ArgumentParser = subParsers.add_parser(
+        name="git", help="Options for interfacing with Git repositories"
+    )
+    gitParser.add_argument(
+        "-s",
+        "--src-directory",
+        default=Path(".").resolve(),
+        type=Path,
+        required=False,
+        help="Directory containing the repository to explode",
+    )
+    gitParser.add_argument(
+        "-d",
+        "--dest-directory",
+        default=Path(".").resolve(),
+        type=Path,
+        required=False,
+        help="Directory to explode into",
+    )
 
     return parser.parse_args()
--- prime_explode/main.py
+++ prime_explode/main.py
@@ -1,4 +1,5 @@
 from argparse import Namespace
+
 from prime_explode.args import getArgs
 
 if __name__ == "__main__":
