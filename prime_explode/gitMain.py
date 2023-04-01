1850b288e009dc590b879a89dae1258652dfc2f5
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-04-01 14:29:20 -0500

    Refactor project for better function names

--- prime_explode/gitMain.py
+++ prime_explode/gitMain.py
@@ -2,13 +2,14 @@ from argparse import Namespace
 from pathlib import Path
 from typing import List, Tuple
 
+from progress.bar import Bar
 from pygit2 import Walker
 
 from prime_explode.utils import filesystem
 from prime_explode.vcs import git
 
 
-def testFileSystem(args: Namespace) -> None:
+def testFileSystem(src: Path, dest: Path) -> None:
     # Steps
 
     # 1. Check if the src directory exists
@@ -17,19 +18,38 @@ def testFileSystem(args: Namespace) -> None:
     # 3. Check if the dest directory exists
     # If it does, exit with reason
 
-    if filesystem.testIfDirectory(path=args.gitSrc) is False:
-        print(f"{args.gitSrc} is not a valid source directory")
+    if filesystem.testIfDirectory(path=src) is False:
+        print(f"{src} is not a valid source directory")
         exit(code=1)
 
-    dotGitFolder: Path = git.getRepoPath(path=args.gitSrc)
+    dotGitFolder: Path = git.getRepoPath(path=src)
 
     if filesystem.testIfDirectory(path=dotGitFolder) is False:
-        print(f"{args.gitSrc.resolve()} is not a valid Git repository")
+        print(f"{src.resolve()} is not a valid Git repository")
         exit(code=2)
 
-    if filesystem.testIfDirectory(args.gitDest):
-        print(f"{args.gitDest.resolve()} already exists.")
+    if filesystem.testIfDirectory(dest):
+        print(f"{dest.resolve()} already exists.")
         exit(code=3)
+
+
+def getCommitWalkers(gitPath: Path) -> List[Tuple[str, Walker]]:
+    commitWalkers: List[Tuple[str, Walker]] = []
+
+    branches: List[str] = git.getBranchesList(path=gitPath)
+
+    with Bar("Getting iterators of commits from each branch", max=len(branches)) as bar:
+        branch: str
+        for branch in branches:
+            git.checkoutBranch(path=gitPath, branch=branch)
+            commitWalker: Walker = git.getCommitWalker(path=gitPath)
+
+            pair: Tuple[str, Walker] = (branch, commitWalker)
+
+            commitWalkers.append(pair)
+            bar.next()
+
+    return commitWalkers
 
 
 def main(args: Namespace) -> None:
@@ -38,20 +58,5 @@ def main(args: Namespace) -> None:
     # 5. Checkout the repository to the HEAD commit
     # 4. Get a list of branches
 
-    testFileSystem(args=args)
-
-    commitWalkers: List[Tuple[str, Walker]] = []
-
-    branches: List[str] = git.getBranchesList(path=args.gitSrc)
-
-    branch: str
-    for branch in branches:
-        git.checkoutBranch(path=args.gitSrc, branch=branch)
-        commitWalker: Walker = git.getCommitWalker(path=args.gitSrc)
-
-        pair: Tuple[str, Walker] = (branch, commitWalker)
-
-        commitWalkers.append(pair)
-
-    for pair in commitWalkers:
-        print(pair)
+    testFileSystem(src=args.gitSrc, dest=args.gitDest)
+    getCommitWalkers(gitPath=args.gitSrc)
